fb80e068fcbdd4b0b03afdea55385c876714fd50
Iftekhar Sakib <pro.iftekhar@gmail.com>
2024-04-16 14:57:24 +0600

    //

--- boolTest.java
+++ boolTest.java
@@ -10,15 +10,18 @@ public class boolTest {
         if (b1 == b2) // false , memory location comparison
             result = 1;
 
-        if (b1.equals(b2)) /*  the equals() method in the Boolean class checks for case-insensitive string comparison. So, this condition evaluates to true. any constructor arguments except true is considered as false*/
+        if (b1.equals(b2)) /*
+                            * the equals() method in the Boolean class checks for case-insensitive string
+                            * comparison. So, this condition evaluates to true. any constructor arguments
+                            * except true is considered as false
+                            */
             result = result * 10;
 
-        if (b2 == b4) //false , memory location comparison
+        if (b2 == b4) // false , memory location comparison
             result = result * 100;
 
         if (b2.equals(b4)) // false
             result = result * 1000;
-
         if (b2.equals(b3)) // true
             result = result * 10000;
 
